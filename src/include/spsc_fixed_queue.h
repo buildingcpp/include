22a5fd355bc07590c28336dad9bf5882c9762b69
michael <michael@michael-maniscalco.com>
2023-09-06 08:59:48 -0400

    add basic spsc queue

---
+++ src/include/spsc_fixed_queue.h
@@ -0,0 +1,152 @@
+#pragma once
+
+#include <concepts>
+#include <cstddef>
+#include <vector>
+
+
+namespace bcpp
+{
+
+    template <typename T>
+    class spsc_fixed_queue : non_copyable
+    {
+    public:
+
+        using type = T;
+
+
+        spsc_fixed_queue
+        (
+            std::size_t
+        );
+
+        spsc_fixed_queue(spsc_fixed_queue &&) = default;
+        spsc_fixed_queue & operator = (spsc_fixed_queue &&) = default;
+        ~spsc_fixed_queue() = default;
+
+        type pop();
+
+        std::size_t try_pop
+        (
+            type &
+        );
+
+        bool push
+        (
+            T &&
+        );
+
+        bool empty() const;
+
+        std::size_t capacity() const;
+
+        std::size_t size() const;
+
+    private:
+
+        std::size_t volatile        front_;
+
+        std::size_t volatile        back_;
+
+        std::size_t                 capacity_;
+        std::size_t                 capacityMask_;
+
+        std::vector<type>           queue_;
+    };
+
+} // namespace bcpp
+
+
+//==============================================================================
+template <typename T>
+bcpp::spsc_fixed_queue<T>::spsc_fixed_queue
+(
+    std::size_t capacity
+):
+    front_(0), 
+    back_(0)
+{
+    capacity_ = 1;
+    while (capacity_ < capacity)
+        capacity_ <<= 1;
+    capacityMask_ = capacity_ - 1;
+    queue_.resize(capacity_);
+}
+
+
+//==============================================================================
+template <typename T>
+inline std::size_t bcpp::spsc_fixed_queue<T>::capacity
+(
+) const
+{
+    return capacity_;
+}
+
+
+//==============================================================================
+template <typename T>
+inline auto bcpp::spsc_fixed_queue<T>::pop
+(
+) -> type
+{
+    std::size_t front = front_;
+    type ret = std::move(queue_[front++ & capacityMask_]);
+    front_ = front;
+    return ret;
+}
+
+
+//==============================================================================
+template <typename T>
+inline std::size_t bcpp::spsc_fixed_queue<T>::try_pop
+(
+    type & value
+)
+{
+    if (auto front = front_, size = (back_ - front); size > 0)
+    {
+        value = std::move(queue_[front++ & capacityMask_]);
+        front_ = front;
+        return size;
+    }
+    return 0;
+}
+
+
+//==============================================================================
+template <typename T>
+inline bool bcpp::spsc_fixed_queue<T>::push
+(
+    T && value
+)
+{
+    if (std::size_t back = back_; (back - front_) < capacity_)
+    {
+        queue_[back++ & capacityMask_] = std::forward<T>(value);
+        back_ = back;
+        return true;
+    }
+    return false;
+}
+
+
+//==============================================================================
+template <typename T>
+inline bool bcpp::spsc_fixed_queue<T>::empty
+(
+) const
+{
+    return (back_ == front_);
+}
+
+
+//==============================================================================
+template <typename T>
+inline std::size_t bcpp::spsc_fixed_queue<T>::size
+(
+) const
+{
+    return (back_ - front_);
+}
