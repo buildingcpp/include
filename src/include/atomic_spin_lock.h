7cf077031446605e7b869a59840edb2cb9c8580c
Michael Maniscalco <michael@buildingcpp.com>
2025-01-20 08:56:31 -0500

    add atomic spin lock

---
+++ src/include/atomic_spin_lock.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <include/non_copyable.h>
+
+#include <thread>
+#include <atomic>
+#include <cstdint>
+
+
+
+namespace bcpp
+{
+
+    class atomic_spin_lock :
+        non_copyable
+    {
+    public:
+
+        using value_type = std::thread::id;
+
+        atomic_spin_lock() = default;
+        atomic_spin_lock
+        (
+            value_type
+        );
+        
+        ~atomic_spin_lock();
+
+        atomic_spin_lock(atomic_spin_lock &&) = default;
+        atomic_spin_lock & operator = (atomic_spin_lock &&) = default;
+
+        void lock();
+
+        void unlock();
+
+        bool try_lock();
+
+
+    private:
+
+        std::atomic<value_type> value_{};
+
+    }; // atomic_spin_lock
+
+} // namespace bcpp
+
+
+//=============================================================================
+inline bcpp::atomic_spin_lock::atomic_spin_lock
+(
+    value_type value
+):
+    value_(value)
+{
+}
+
+
+//=============================================================================
+inline bcpp::atomic_spin_lock::~atomic_spin_lock
+(
+)
+{
+    static value_type const invalid_value = {};
+    auto expected = std::this_thread::get_id();
+    value_.compare_exchange_strong(expected, invalid_value);
+}
+
+
+//=============================================================================
+inline void bcpp::atomic_spin_lock::lock
+(
+)
+{
+    static value_type const invalid_value = {};
+    auto expected = invalid_value;
+    auto desired = std::this_thread::get_id();
+    while (not value_.compare_exchange_strong(expected, desired))
+        ;
+}
+
+
+//=============================================================================
+inline void bcpp::atomic_spin_lock::unlock
+(
+)
+{
+    static value_type const invalid_value = {};
+    auto expected = std::this_thread::get_id();
+    value_.compare_exchange_strong(expected, invalid_value);
+}
+
+
+//=============================================================================
+inline bool bcpp::atomic_spin_lock::try_lock
+(
+)
+{
+    static value_type const invalid_value = {};
+    auto expected = invalid_value;
+    auto desired = std::this_thread::get_id();
+    return value_.compare_exchange_strong(expected, desired);
+}
